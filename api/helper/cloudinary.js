bafcaa1f6c65eca2e02232682535e176f1c84258
Thao Nguyen <69065535+thaonguyen248@users.noreply.github.com>
2024-11-29 11:46:40 +0700

    Add files via upload

---
+++ api/helper/cloudinary.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+                                cloud_name: process.env.cloudinary_Config_Cloud_Name,
+                                api_key: process.env.cloudinary_Config_api_key,
+                                api_secret: process.env.cloudinary_Config_api_secret
+                                // secure: true
+                                });
+
+module.exports = cloudinary;
